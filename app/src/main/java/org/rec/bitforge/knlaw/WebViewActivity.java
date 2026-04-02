3f1ec70f91f11fde162eae3e3a51b1ab0b201650
SteelSocket <steelsocket@gmail.com>
2026-04-02 19:16:34 +0530

    Added Basic Webview

---
+++ app/src/main/java/org/rec/bitforge/knlaw/WebViewActivity.java
@@ -0,0 +1,22 @@
+package org.rec.bitforge.knlaw;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
+    }
+}
