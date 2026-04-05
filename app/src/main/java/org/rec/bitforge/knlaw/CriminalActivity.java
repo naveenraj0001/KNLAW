ad0c21d17df05844b0486a54ddf34d162fc7a017
Naveen Raj <naveenraj.s.2024.cse@rajalakshmi.edu.in>
2026-04-05 13:18:45 +0530

    local changes before pulling

--- app/src/main/java/org/rec/bitforge/knlaw/CriminalActivity.java
+++ app/src/main/java/org/rec/bitforge/knlaw/CriminalActivity.java
@@ -15,7 +15,6 @@ public class CriminalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_criminal);
-        setTitle("Criminal Laws");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
